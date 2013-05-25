cbd11d9d56f90dc79896bd2e702a36af6e2b9cb7
ka <ka.kaosf@gmail.com>
2013-05-25 16:03:57 +0900

    add assets/index.html and show it on WebView

--- src/net/kaosfield/wv2/MainActivity.java
+++ src/net/kaosfield/wv2/MainActivity.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+import android.webkit.WebView;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView wv = (WebView) findViewById(R.id.webview);
+        wv.loadUrl("file:///android_asset/index.html");
     }
 
 
