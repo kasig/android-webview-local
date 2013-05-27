70ada60308118a9adebe6c7841df4b9bb0bdba48
ka <ka.kaosf@gmail.com>
2013-05-27 17:40:34 +0900

    clean up source code

--- src/net/kaosfield/wv2/MainActivity.java
+++ src/net/kaosfield/wv2/MainActivity.java
@@ -9,7 +9,6 @@ import android.webkit.WebSettings;
 import android.webkit.WebChromeClient;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +24,10 @@ public class MainActivity extends Activity {
         wv.loadUrl("file:///android_asset/index.html");
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
 }
